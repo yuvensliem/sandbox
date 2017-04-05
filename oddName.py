b0b68774fd6c004bbca56c9579ebed10646647d3
yuvensliem <yuvensenverd@yahoo.com>
2017-04-05 10:21:15 +0800

    add error checking

--- oddName.py
+++ oddName.py
@@ -3,3 +3,9 @@ Yuvens Enverd Liem
 Add error checking
 sadjiasdjaisd
 """
+
+name = str(input("Please enter your name :"))
+x = len(name)
+while x == 0 :
+    name = str(input("Please enter your name :"))
+print(name[1])
