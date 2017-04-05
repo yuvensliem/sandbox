b81802e650e3859e2932a3b204a7026977229d12
yuvensliem <yuvensenverd@yahoo.com>
2017-04-05 11:35:45 +0800

    chnges error

--- oddName.py
+++ oddName.py
@@ -4,11 +4,17 @@ Add error checking
 sadjiasdjaisd
 """
 def main():
+    get_name()
+
+
+def get_name():
     name = str(input("Please enter your name :"))
     x = len(name)
-    while x == 0 :
+    while x == 0:
         name = str(input("Please enter your name :"))
-    print(name[1])
+    for i in range (0, x, 2):
+        print(name[i])
+
 
 main()
 """
